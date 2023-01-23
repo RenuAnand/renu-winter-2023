734b184f49a834b931cb016caa7ed32b2011ffd8
Renu Sharma <renuanand.sharma7@gmail.com>
2023-01-22 23:59:17 -0500

    homework2

---
+++ Modules/Class2/homework2.js
@@ -0,0 +1,11 @@
+let sentance1 = "go to home";
+let lengthofsentence = 150;
+let startwith = "go to";
+let nothing = "";
+
+let result = lengthofsentence > 100 && startwith == "go to" ? 25 : 35;
+console.log(`\n sentance -> ${sentance1}`);
+console.log(`\n length of sentence > 100->${lengthofsentence > 100}`);
+console.log(`\n startwith ->${startwith === "go to"}`);
+console.log(`\n nothing->${nothing}`);
+console.log(`\n result ->${result}`);
